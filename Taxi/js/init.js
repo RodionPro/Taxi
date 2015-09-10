903208bc3498da19990de70422bfdc04aed1273c
flaprogrammer <flaprogrammer@gmail.com>
2015-09-10 22:25:00 +0300

    вторая шапка кнопка qr кода

--- Taxi/js/init.js
+++ Taxi/js/init.js
@@ -1,11 +1,36 @@
 $(document).ready(function() {
 
+	/* HEADER */
+
+	$(window).on('scroll', function(e) {
+		var t = $(window).scrollTop();
+		var $mainHead = $('#headBar');
+		var $fixedHead = $('#headBar2');
+		var $nav = $('.content .nav');
+
+		if(t>0 && !$mainHead.hasClass('headBar--invis')) {
+			$mainHead.addClass('headBar--invis');
+			$nav.addClass('nav--invis');
+			$fixedHead.removeClass('headBar2--invis');
+			$(window).scrollTop(1);
+		}
+		else if(t==0) {
+			$mainHead.removeClass('headBar--invis');
+			$nav.removeClass('nav--invis');
+			$fixedHead.addClass('headBar2--invis');
+		}
+	});
+
+
+	/* HEADER END */
+
   /* QR КОДЫ */
-  $(".cover").click(function () {
-    $(this).children(".spoiler_indicator").toggleClass("res");
-    $(this).children(".img1").toggleClass("close");
-    $(this).children(".img2").toggleClass("open");
-  });
+	$(".cover .spoiler_indicator").click(function () {
+		var $el = $(this).closest('.cover');
+		$el.children(".spoiler_indicator").toggleClass("res");
+		$el.children(".img1").toggleClass("close");
+		$el.children(".img2").toggleClass("open");
+	});
   /* QR КОДЫ */
 
   /* ВЫБОР СТРАНЫ */
@@ -15,19 +40,19 @@ $(document).ready(function() {
   /* ВЫБОР СТРАНЫ */  
 
   /* ПОП АП ВИДЕО */  
-  p = $('.popup__overlay')
+  p = $('.popup__overlay');
   $('#popup_video').click(function() {
     p.css('display', 'block');
-  })
+  });
   p.click(function(event) {
-    e = event || window.event
+    e = event || window.event;
     if (e.target == this) {
       $(p).css('display', 'none')
     }
-  })
+  });
   $('.popup__close').click(function() {
     p.css('display', 'none')
-  })  
+  });
   /* ПОП АП ВИДЕО */    
 
 
