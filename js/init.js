d02cdb23b79720714a1e87fc0dc2e1246faba09e
Rad Cor <rodion_b@ukr.net>
2015-09-11 14:42:50 +0300

    Правка адаптивного меню и добавление его на внутренние странички

--- js/init.js
+++ js/init.js
@@ -1,29 +1,4 @@
 $(document).ready(function() {
-
-	/* HEADER */
-
-	$(window).on('scroll', function(e) {
-		var t = $(window).scrollTop();
-		var $mainHead = $('.headBar__wrapper');
-		var $fixedHead = $('#headBar2');
-		var $nav = $('.content .nav');
-
-		if(t>0 && !$mainHead.hasClass('headBar__wrapper--invis')) {
-			$mainHead.addClass('headBar__wrapper--invis');
-			$nav.addClass('nav--invis');
-			$fixedHead.removeClass('headBar2--invis');
-			$(window).scrollTop(5);
-		}
-		else if(t==0) {
-			$mainHead.removeClass('headBar__wrapper--invis');
-			$nav.removeClass('nav--invis');
-			$fixedHead.addClass('headBar2--invis');
-		}
-	});
-
-
-	/* HEADER END */
-
   /* QR КОДЫ */
 	$(".cover .spoiler_indicator").click(function () {
 		var $el = $(this).closest('.cover');
