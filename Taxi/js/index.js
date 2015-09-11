466063e62aaca6e375595195fcc257549aee818b
flaprogrammer <flaprogrammer@gmail.com>
2015-09-11 23:51:54 +0300

    при скролле не дает скроллить страницу

--- Taxi/js/index.js
+++ Taxi/js/index.js
@@ -2,22 +2,33 @@ $(document).ready(function() {
 
 	/* HEADER */
 
+	var scrolling = false;
+
 	$(window).on('scroll', function (e) {
 		var t = $(window).scrollTop();
 		var $mainHead = $('.headBar__wrapper');
 		var $fixedHead = $('#headBar2');
 		var $nav = $('.content .nav');
+		//if(scrolling) {
+			$(window).scrollTop(5);
+		//}
 
-		if (t > 0 && !$mainHead.hasClass('headBar__wrapper--invis')) {
+		/*if (!$mainHead.hasClass('headBar__wrapper--invis')) {
 			$mainHead.addClass('headBar__wrapper--invis');
 			$nav.addClass('nav--invis');
 			$fixedHead.removeClass('headBar2--invis');
 			$(window).scrollTop(5);
+
+			scrolling = true;
+
+			setTimeout(function() {
+				scrolling = false;
+			}, 1000);
 		}
 		else if (t == 0) {
 			$mainHead.removeClass('headBar__wrapper--invis');
 			$nav.removeClass('nav--invis');
 			$fixedHead.addClass('headBar2--invis');
-		}
+		}*/
 	});
 });
--- js/index.js
+++ js/index.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
 
+	var scrolling = false;
 	/* HEADER */
 	$(window).on('scroll', function(e) {
+		if($('.headBar__wrapper').css('display')=='none') return;
 		var t = $(window).scrollTop();
 		var $mainHead = $('.headBar__wrapper');
 		var $fixedHead = $('#headBar2');
 		var $nav = $('.content .nav');
+
+		if(scrolling) {
+		$(window).scrollTop(5);
+		}
 
 		if(t>0 && !$mainHead.hasClass('headBar__wrapper--invis')) {
 			$mainHead.addClass('headBar__wrapper--invis');
 			$nav.addClass('nav--invis');
 			$fixedHead.removeClass('headBar2--invis');
 			$(window).scrollTop(5);
+			scrolling = true;
+			setTimeout(function() {
+				scrolling = false;
+			}, 1000);
 		}
 		else if(t==0) {
 			$mainHead.removeClass('headBar__wrapper--invis');
