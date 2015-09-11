27125c4ba282810a967da4764e03d4d2fc1f6ca5
flaprogrammer <flaprogrammer@gmail.com>
2015-09-11 10:37:22 +0300

    fix баг с обрезанной собачкой

--- Taxi/js/init.js
+++ Taxi/js/init.js
@@ -4,18 +4,18 @@ $(document).ready(function() {
 
 	$(window).on('scroll', function(e) {
 		var t = $(window).scrollTop();
-		var $mainHead = $('#headBar');
+		var $mainHead = $('.headBar__wrapper');
 		var $fixedHead = $('#headBar2');
 		var $nav = $('.content .nav');
 
-		if(t>0 && !$mainHead.hasClass('headBar--invis')) {
-			$mainHead.addClass('headBar--invis');
+		if(t>0 && !$mainHead.hasClass('headBar__wrapper--invis')) {
+			$mainHead.addClass('headBar__wrapper--invis');
 			$nav.addClass('nav--invis');
 			$fixedHead.removeClass('headBar2--invis');
 			$(window).scrollTop(1);
 		}
 		else if(t==0) {
-			$mainHead.removeClass('headBar--invis');
+			$mainHead.removeClass('headBar__wrapper--invis');
 			$nav.removeClass('nav--invis');
 			$fixedHead.addClass('headBar2--invis');
 		}
